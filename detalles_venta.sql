e904ef86c40b196058ec2db8b5f9f013ed1ec35d
Perlita <160614177+PEPERJHGFD@users.noreply.github.com>
2024-04-15 12:04:11 -0600

    Create detalles_venta.sql

---
+++ detalles_venta.sql
@@ -0,0 +1,9 @@
+-- Forma 1: todos los campos de forma manual
+CREATE TABLE IF NOT EXISTS detalle_ventas(
+    id_detalle_venta integer PRIMARY KEY AUTOINCREMENT,
+    id_venta integer REFERENCES ventas(id_venta),
+    id_producto integer REFERENCES productos(id_producto),
+    cantidad_producto integer,
+    precio_unitario float,
+    total_x_producto float
+);
